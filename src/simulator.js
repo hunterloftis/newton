9b4432e947d24103c52499d5725d0f636c16fba4
Hunter Loftis <hunter@hunterloftis.com>
2013-10-25 12:13:20 -0400

    moved requestanimationframe shim out

--- src/simulator.js
+++ src/simulator.js
@@ -1,30 +1,5 @@
 ;(function() {
 
-  (function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = function(simulator, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { simulator(currTime + timeToCall); },
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-    }
-  })();
-
   function Simulator(simulator, renderer, integrationFps) {
     if (!(this instanceof Simulator)) return new Simulator(simulator, renderer, integrationFps);
     this.simulator = simulator;
