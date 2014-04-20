36eab046d9be83e77e2554bd668a97af89d0af43
Hunter Loftis <hunter@recurly.com>
2014-04-19 18:47:24 -0700

    tests for Constraint

---
+++ spec/constraint.spec.js
@@ -0,0 +1,30 @@
+var assert = require('chai').assert;
+var Constraint = require('../lib/constraint');
+
+describe('Constraint', function() {
+
+  describe('.setPriority()', function() {
+
+    function Foo() {}
+    function Bar() { Constraint.call(this); }
+    Bar.prototype = Object.create(Constraint.prototype);
+
+    it('should set Infinity with no matches', function() {
+      var c = Constraint();
+      c.setPriority([ Foo, Bar ]);
+      assert.equal(c.priority, Infinity);
+    });
+
+    it('should set zero as the first match', function() {
+      var c = Constraint();
+      c.setPriority([ Constraint, Foo, Bar ]);
+      assert.equal(c.priority, 0);
+    });
+
+    it('should set 2 as the third match', function() {
+      var c = Constraint();
+      c.setPriority([ Foo, Bar, Constraint ]);
+      assert.equal(c.priority, 2);
+    });
+  });
+});
